6816588c47589e874d5ad52139413814ddbecdd0
eckoqzhang <eckoqzhang@tencent.com>
2021-01-30 10:57:57 +0800

    feat(supervisord): change conf

--- start.sh
+++ start.sh
@@ -1,10 +1,21 @@
 #!/bin/sh
 
-yum install gcc
-yum install python3
-yum install python-devel
-yum install python3-devel
-pip3 install configobj
-pip3 install psutil
-pip3 install requests
-pip3 install supervisor
+exist=$(ps -ef |grep supervisord |grep -v grep |wc -l)
+if [[ $exist -eq 1 ]]; then
+    echo "[info] supervisord exist. reload it now"
+    /usr/local/bin/supervisorctl reload
+else
+    echo "[info] supervisord start"
+    /usr/local/bin/supervisord -c /agent-monitor-master/supervisord.conf
+fi
+
+sleep 1
+
+status=$(ps -ef |grep agent_monitor |grep -v grep|wc -l)
+if [[ $status -eq 1 ]];then
+   echo "[info] agent_monitor exist. exit now"
+   exit 0
+fi
+
+echo "[info] supervisord exist. restart agent_monitor"
+/usr/local/bin/supervisorctl restart agent_monitor
